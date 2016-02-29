8ee170eff875f47525a8f9703d8d905634c48c6c
Alejandro <alexro2404@hotmail.com>
2016-02-28 20:29:36 -0600

    Agregando metodo Objeto bundle para recuperar parametros de la actividad anterior

--- src/com/sencide/MenuApplication.java
+++ src/com/sencide/MenuApplication.java
@@ -1,16 +1,28 @@
 package com.sencide;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MenuApplication extends Activity{
+	private String urlFinSession="https://ayamictlan.uam.mx:8443/sae/azc/AEWBU005.oFinSesion";
 	@Override
     public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.menuapplication);
+        Bundle bundle = getIntent().getExtras();
      }
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -18,6 +30,49 @@ public class MenuApplication extends Activity{
 		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
 	}
+	
+	public void postLoginData() {
+    	HttpResponse response = null;
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        
+        
+        HttpPost httppost = new HttpPost(urlFinSession);
+
+        try {
+                    	
+            // Execute HTTP Post Request
+            Log.w("SENCIDE", "Execute HTTP Post Request");
+            response = httpclient.execute(httppost);
+            
+            String str = inputStreamToString(response.getEntity().getContent()).toString();
+            
+            Log.w("SENCIDE", str);
+            
+        } catch (ClientProtocolException e) {
+        	e.printStackTrace();
+        } catch (IOException e) {
+        	e.printStackTrace();
+        }
+        //Header[] cookies = responsePost.getHeaders("Set-Cookie");
+    } 
+  
+    private StringBuilder inputStreamToString(InputStream is) {
+    	String line = "";
+    	StringBuilder total = new StringBuilder();
+    	// Wrap a BufferedReader around the InputStream
+    	BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+    	// Read response until the end
+    	try {
+			while ((line = rd.readLine()) != null) { 
+				total.append(line); 
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	// Return full string
+    	return total;
+    }
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -26,7 +81,8 @@ public class MenuApplication extends Activity{
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_logout) {
-			return true;
+			postLoginData();
+			//return true;
 		}
 		if (id == R.id.action_help) {
 			return true;
