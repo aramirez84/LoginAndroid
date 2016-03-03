84cae6ee828702de1dac425671e8264b87205087
Alejandro Ramirez Osorio <alejandro.ramirez@scitum.com.mx>
2016-03-03 15:27:52 -0600

    Renombrando archivo

--- src/com/sencide/Biblioteca.java
+++ src/com/sencide/Biblioteca.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class Principal extends Activity
+public class Biblioteca extends Activity
 {
 	private List<String> cookies;
     @Override
