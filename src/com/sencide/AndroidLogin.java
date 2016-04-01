d9eeaa496ee59140f681ee84b3388f83ca18a091
Alejandro Ramirez Osorio <alejandro.ramirez@scitum.com.mx>
2016-03-31 19:06:27 -0600

    Agregando llanada Asyncrona para el modulo de login

--- src/com/sencide/AndroidLogin.java
+++ src/com/sencide/AndroidLogin.java
@@ -26,18 +26,23 @@ import org.apache.http.util.EntityUtils;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.ParseException;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AndroidLogin extends Activity implements OnClickListener {
 	
 	private Button ok;
-	private TextView result;
+	private TextView alertas;
+	private ProgressBar pb;
+	private EditText uname,pword;
 	private String urlLogin="https://ayamictlan.uam.mx:8443/sae/azc/AEWBU004.oIniSesWebLic?mod=1";
 	String urlHorario="https://ayamictlan.uam.mx:8443/sae/azc/IEWBC002.oConsulta";
 	Header coookies2;
@@ -46,77 +51,39 @@ public class AndroidLogin extends Activity implements OnClickListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+        uname = (EditText)findViewById(R.id.txt_username);
+    	pword = (EditText)findViewById(R.id.txt_password);
+    	
+		pb=(ProgressBar)findViewById(R.id.progressBar1);
+		pb.setVisibility(View.GONE);
         // Login button clicked
         ok = (Button)findViewById(R.id.btn_login);
         ok.setOnClickListener(this);
         
-        result = (TextView)findViewById(R.id.lbl_result);
+        alertas = (TextView)findViewById(R.id.lbl_result);
         
     }
     
-    
-    public void postLoginData() {
-    	HttpResponse response = null;
+    @Override
+	public void onClick(View view) {
+		if(view == ok){
+			Toast.makeText(getApplicationContext(), "Validando datos....", Toast.LENGTH_SHORT).show();
+			pb.setVisibility(View.VISIBLE);
+			new MyAsyncTask().execute(urlLogin);
+		}
+	}
+	
+    /*public void postLoginData() {
+    	
         // Create a new HttpClient and Post Header
         HttpClient httpclient = new DefaultHttpClient();
         
         
-        HttpPost httppost = new HttpPost(urlLogin);
+        //HttpPost httppost = new HttpPost(urlLogin);
 
         try {
             // Add user name and password
-        	EditText uname = (EditText)findViewById(R.id.txt_username);
-        	String username = uname.getText().toString();
-
-        	EditText pword = (EditText)findViewById(R.id.txt_password);
-        	String password = pword.getText().toString();
         	
-            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-            nameValuePairs.add(new BasicNameValuePair("SIGLAS_UNI_XX.E_UNIDAD.AE02.1","AZC"));
-            nameValuePairs.add(new BasicNameValuePair("%23.E_UNIDAD.AE02.1","AxJDMQ=="));
-            nameValuePairs.add(new BasicNameValuePair("%23CRC.E_UNIDAD.AE02.1","0000006B"));
-            nameValuePairs.add(new BasicNameValuePair("NOMBRE.IDENTIFICACION.NONMODELED", username));
-            nameValuePairs.add(new BasicNameValuePair("COMPLEMENTO.IDENTIFICACION.NONMODELED", password));
-            nameValuePairs.add(new BasicNameValuePair("GO.IDENTIFICACION.NONMODELED","Entrar"));
-            nameValuePairs.add(new BasicNameValuePair("%25.IDENTIFICACION.NONMODELED.1",""));
-            nameValuePairs.add(new BasicNameValuePair("%23.WEB_INFO.SW01.1","CxJDMjA5MzMwMDcy"));
-            nameValuePairs.add(new BasicNameValuePair("%23CRC.WEB_INFO.SW01.1","00000022"));
-            nameValuePairs.add(new BasicNameValuePair("%23.WEB_MOD_ASO.SW01.1",""));
-            nameValuePairs.add(new BasicNameValuePair("%23.USUARIO_ANEXO.SG02.1",""));
-            nameValuePairs.add(new BasicNameValuePair("%23.MODULO_UWAS.SAE01.1",""));
-            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-            // Execute HTTP Post Request
-            Log.w("SENCIDE", "Execute HTTP Post Request");
-            response = httpclient.execute(httppost);
-            String str = inputStreamToString(response.getEntity().getContent()).toString();
-            //Filtramos el atributo onload que nos da la valdiacion del formulario
-            Pattern pattern = Pattern.compile("(\\(.*?)\\)");
-            List<String> mensajes=getMensaje(pattern, str);
-            //Hacemos un segundo filto para obtener solo el mensaje de validacion del formualrio
-            Pattern pattern2 = Pattern.compile("[A-Z].*[a-z]");
-            List<String> mensajes2=getMensaje(pattern2, mensajes.get(1));
-            
-            Log.w("ALERTA",mensajes2.toString());
-            //Log.w("SENCIDE", str);
-            
-            if(mensajes.size()==2)
-            {
-            	//cookies = etCookies(response);
-            	Log.w("SENCIDE", "TRUE");
-            	result.setText("Login successful");
-            	Intent intent = new Intent(AndroidLogin.this, MenuApplication.class);
-            	startActivity(intent);
-            	uname.setText("");
-            	pword.setText("");
-            	result.setText("");
-            }else
-            {
-            	Log.w("SENCIDE", "FALSE");
-            	result.setText(mensajes2.toString());            	
-            }
-
         } catch (ClientProtocolException e) {
         	e.printStackTrace();
         } catch (IOException e) {
@@ -134,7 +101,7 @@ public class AndroidLogin extends Activity implements OnClickListener {
             e.printStackTrace();
         }
             } 
-     
+     */
     private StringBuilder inputStreamToString(InputStream is) {
     	String line = "";
     	StringBuilder total = new StringBuilder();
@@ -152,12 +119,6 @@ public class AndroidLogin extends Activity implements OnClickListener {
     	return total;
     }
 
-	@Override
-	public void onClick(View view) {
-		if(view == ok){
-			postLoginData();
-		}
-	}
 	
 	/*
 	 * Metodo para filtrar contenido
@@ -309,6 +270,101 @@ public class AndroidLogin extends Activity implements OnClickListener {
 	        */
 
 	    } 
+	private class MyAsyncTask extends AsyncTask<String, Void, String>
+	{
+		
+		 
+		@Override
+		protected String doInBackground(String... params)
+		{
+			HttpResponse response = null;
+			List<String> mensajes2=null;
+			String resultPost=null;
+			// Create a new HttpClient and Post Header
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost(params[0]);
+ 
+			try {
+				String username = uname.getText().toString();
+				String password = pword.getText().toString();
+	        	
+	            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+	            nameValuePairs.add(new BasicNameValuePair("SIGLAS_UNI_XX.E_UNIDAD.AE02.1","AZC"));
+	            nameValuePairs.add(new BasicNameValuePair("%23.E_UNIDAD.AE02.1","AxJDMQ=="));
+	            nameValuePairs.add(new BasicNameValuePair("%23CRC.E_UNIDAD.AE02.1","0000006B"));
+	            nameValuePairs.add(new BasicNameValuePair("NOMBRE.IDENTIFICACION.NONMODELED", username));
+	            nameValuePairs.add(new BasicNameValuePair("COMPLEMENTO.IDENTIFICACION.NONMODELED", password));
+	            nameValuePairs.add(new BasicNameValuePair("GO.IDENTIFICACION.NONMODELED","Entrar"));
+	            nameValuePairs.add(new BasicNameValuePair("%25.IDENTIFICACION.NONMODELED.1",""));
+	            nameValuePairs.add(new BasicNameValuePair("%23.WEB_INFO.SW01.1","CxJDMjA5MzMwMDcy"));
+	            nameValuePairs.add(new BasicNameValuePair("%23CRC.WEB_INFO.SW01.1","00000022"));
+	            nameValuePairs.add(new BasicNameValuePair("%23.WEB_MOD_ASO.SW01.1",""));
+	            nameValuePairs.add(new BasicNameValuePair("%23.USUARIO_ANEXO.SG02.1",""));
+	            nameValuePairs.add(new BasicNameValuePair("%23.MODULO_UWAS.SAE01.1",""));
+	            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+	            // Execute HTTP Post Request
+	            Log.w("SENCIDE", "Execute HTTP Post Request");
+	            response = httpclient.execute(httppost);
+	            String str = inputStreamToString(response.getEntity().getContent()).toString();
+	            //Filtramos el atributo onload que nos da la valdiacion del formulario
+	            Pattern pattern = Pattern.compile("(\\(.*?)\\)");
+	            List<String> mensajes=getMensaje(pattern, str);
+	            //Hacemos un segundo filto para obtener solo el mensaje de validacion del formualrio
+	            Pattern pattern2 = Pattern.compile("[A-Z].*[a-z]");
+	            mensajes2=getMensaje(pattern2, mensajes.get(1));
+	            
+	            Log.w("ALERTA",mensajes2.toString());
+	            //Log.w("SENCIDE", str);
+	            
+	            if(mensajes.size()==2)
+	            {
+	            	//cookies = etCookies(response);
+	            	/*Log.w("SENCIDE", "TRUE");
+	            	result.setText("Login successful");
+	            	Intent intent = new Intent(AndroidLogin.this, MenuApplication.class);
+	            	startActivity(intent);
+	            	uname.setText("");
+	            	pword.setText("");
+	            	result.setText("");*/
+	            	resultPost=Integer.toString(mensajes.size());
+	            }else
+	            {
+	            	/*
+	            	Log.w("SENCIDE", "FALSE");
+	            	result.setText(mensajes2.toString());*/
+	            	resultPost=mensajes2.toString();
+	            }
+
+ 
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+			}
+			return resultPost;
+			
+		}
+ 
+		protected void onPostExecute(String result)
+		{
+			pb.setVisibility(View.GONE);
+			if(result.equals("2"))
+            {
+            	//cookies = etCookies(response);
+            	Log.w("SENCIDE", "TRUE");
+            	Intent intent = new Intent(AndroidLogin.this, MenuApplication.class);
+            	startActivity(intent);
+            	uname.setText("");
+            	pword.setText("");
+            }else
+            {
+            	Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();            	
+            }
+		}
+		
+ 
+	}
 
 	
 }
