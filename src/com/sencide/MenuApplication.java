2ffc286dd08dabbdc19fcfa97cbda5faba6e19c3
Alejandro <alexro2404@hotmail.com>
2016-04-02 15:36:07 -0600

    Agregando tarea asincrona para cada boton del menu de la app

--- src/com/sencide/MenuApplication.java
+++ src/com/sencide/MenuApplication.java
@@ -1,24 +1,16 @@
 package com.sencide;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -26,11 +18,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MenuApplication extends Activity implements OnClickListener{
 	private String urlFinSession="https://ayamictlan.uam.mx:8443/sae/azc/AEWBU005.oFinSesion";
 	
-	private List<String> cookies;
+	//private List<String> cookies;
+	private ProgressBar pb;
 	private Button horario,biblioteca,avisos,contactos;
 	
 	@Override
@@ -38,7 +33,8 @@ public class MenuApplication extends Activity implements OnClickListener{
 	{
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.menuapplication);
-        Bundle bundle = getIntent().getExtras();
+        pb=(ProgressBar)findViewById(R.id.progressBar1);
+		pb.setVisibility(View.GONE);
         horario = (Button)findViewById(R.id.btn_horario);
         biblioteca=(Button)findViewById(R.id.btn_biblioteca);
         avisos=(Button)findViewById(R.id.btn_avisos);
@@ -48,12 +44,38 @@ public class MenuApplication extends Activity implements OnClickListener{
         avisos.setOnClickListener(this);
         contactos.setOnClickListener(this);
         
-     }
+    }
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
+	}
+	@Override
+	public void onClick(View view) {
+		String nameButton=null;
+		
+		if(view == horario)
+		{
+			nameButton=(String) horario.getText();
+			Toast.makeText(getApplicationContext(), "Obteniendo Horario....", Toast.LENGTH_SHORT).show();
+			pb.setVisibility(View.VISIBLE);
+			new MyAsyncTask().execute(nameButton);
+		}
+		if(view == avisos)
+		{
+			nameButton=(String) avisos.getText();
+			Toast.makeText(getApplicationContext(), "Obteniendo Avisos....", Toast.LENGTH_SHORT).show();
+			pb.setVisibility(View.VISIBLE);
+			new MyAsyncTask().execute(nameButton);
+		}
+		if(view == biblioteca)
+		{
+			nameButton=(String) biblioteca.getText();
+			Toast.makeText(getApplicationContext(), "Conectando ....", Toast.LENGTH_SHORT).show();
+			pb.setVisibility(View.VISIBLE);
+			new MyAsyncTask().execute(nameButton);
+		}
 	}
 	 
 
@@ -95,24 +117,40 @@ public class MenuApplication extends Activity implements OnClickListener{
 		return super.onOptionsItemSelected(item);
 	}
 	
-	@Override
-	public void onClick(View view) {
+	private class MyAsyncTask extends AsyncTask<String, Void, String>
+	{
 		
-		if(view == horario)
+		 
+		@Override
+		protected String doInBackground(String... params)
 		{
-			//Intent intent = new Intent(MenuApplication.this, Horario.class);
-        	//intent.putStringArrayListExtra("cookies", (ArrayList<String>) cookies);
-        	//startActivity(intent);
+			String resultButton=null;
+			resultButton=params[0];
+			return resultButton;
+			
 		}
-		if(view == avisos)
+ 
+		protected void onPostExecute(String result)
 		{
-			Intent intent = new Intent(MenuApplication.this, Avisos.class);
-        	startActivity(intent);
+			pb.setVisibility(View.GONE);
+			if(result.equals("Horario"))
+            {
+				//Intent intent = new Intent(MenuApplication.this, Horario.class);
+	        	//intent.putStringArrayListExtra("cookies", (ArrayList<String>) cookies);
+	        	//startActivity(intent);
+            }
+			if(result.equals("Avisos de la Universidad"))
+            {
+            	Intent intent = new Intent(MenuApplication.this, Avisos.class);
+            	startActivity(intent);
+            }
+			if(result.equals("Biblioteca"))
+            {
+            	Intent intent = new Intent(MenuApplication.this, BibliotecaLogin.class);
+            	startActivity(intent);
+            }
 		}
-		if(view == biblioteca)
-		{
-			Intent intent = new Intent(MenuApplication.this, BibliotecaLogin.class);
-        	startActivity(intent);
-		}
-	}	
+		
+ 
+	}
 }
